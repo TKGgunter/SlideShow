a176ba25d6aefa8106640318d67c9950cc116c9d
TKGgunter <thothgunter@live.com>
2018-03-23 10:11:54 -0500

    added wrap functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate printpdf;
+extern crate freetype;
 
 use printpdf::*;
 use std::fs::File;
@@ -7,12 +8,72 @@ use std::io::BufWriter;
 mod parser;
 use parser::{Card, ConfigCard, SlideCard, ConfigKwds, ValueType, example};
 
+
+/// Calculates the lower left of a centered string, to position it correctly in the document
+/// 
+/// Parameters:
+/// 
+/// - text: The actual text that will be embedded in the document
+/// - font_scale: The current scale of the font
+/// - parent_width: the width of the element in which relation the text should be centered
+/// - font_face: The font-face of the text
+/// From printpdf author 
+
+
+pub fn calc_lower_left_for_centered_text(text: &String, font_scale: i64, parent_width: f64, font_face: &freetype::Face)
+-> f64
+{
+    let s_w = calc_text_width_pt(text, font_scale, font_face);
+    ((mm_to_pt!(parent_width) / 2.0) - (s_w / 2.0)) * 0.352778
+}
+
+
+
+#[allow(unused_variables)]
+pub fn calc_text_width_pt(text: &String, font_scale: i64, font_face: &freetype::Face)
+-> f64
+{
+    // vertical scale for the space character
+    let vert_scale = {
+        if let Ok(ch) = font_face.load_char(0x0020, freetype::face::LoadFlag::NO_SCALE) {
+            font_face.glyph().metrics().vertAdvance
+        } else {
+            1000
+        }
+    };
+
+    // calculate the width of the text in unscaled units
+    let sum_width = text.chars().fold(0, |acc, ch| 
+        if let Ok(ch) = font_face.load_char(ch as usize, freetype::face::LoadFlag::NO_SCALE) {
+            let glyph_w = font_face.glyph().metrics().horiAdvance;
+            acc + glyph_w
+        } else { acc }
+    );
+
+    sum_width as f64 / (vert_scale as f64 / font_scale as f64)
+}
+
+
+fn render_centered_text( current_layer: &PdfLayerReference, text: &String, font_scale: i64, canvas_width: f64, y_mm: f64, face: &freetype::Face, font: &IndirectFontRef){
+
+    let centered = calc_lower_left_for_centered_text(text, font_scale, canvas_width, face);
+    current_layer.use_text(&text[..], font_scale, centered, y_mm, font);
+  
+}
+
+
+
+
+
+
+
 //Need to take care of \n
 fn main() {
     //DEFAULT SETINGS
     //16:9
     let mut dimensions = (Mm(338.7), Mm(190.5));
-    let default_font_data = File::open("/home/tgunter/Rust/SlideShow/assets/Roboto-Medium.ttf").unwrap();
+    //let default_font_data = File::open("/home/tgunter/Rust/SlideShow/assets/Roboto-Medium.ttf").unwrap();
+    let default_font_data = File::open("/home/gunter/Rust/Projects/SlideShow/assets/lmroman6-regular.otf").unwrap();
     let mut default_slide_color = [256.0, 256.0, 256.0];
     let default_font_color = [0.0, 0.0, 0.0];
 
